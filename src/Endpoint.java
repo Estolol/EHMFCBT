ce71323bcb7bbf32e8543d176dc6fd6b51dae422
Lillypucien <bon_ariane@hotmail.fr>
2017-02-23 19:59:20 +0100

    r

--- src/Endpoint.java
+++ src/Endpoint.java
@@ -12,12 +12,28 @@ public class Endpoint{
   int Ld;
   int nb_cache;
   HashMap<Integer, Integer> caches;
+  LinkedList<Integer> sorted_caches;
+  int current_cache_pos;
 
   public Endpoint(int Ld, int nb_cache){
     this.Ld = Ld;
     this.nb_cache = nb_cache;
     caches = new HashMap<Integer, Integer>(nb_cache);
+    sorted_caches = new LinkedList<Integer>;
   }
 
-  
+  public void add(int cache_id, int latency){
+    caches.add(cache_id, latency);
+    sorted_caches.add(cache_id);
+  }
+
+  public void sort_by_cache_size(){
+    Collections.sort(sorted_caches,
+      new Comparator<Integer>{
+        public int compare(int a, int b){
+          return Integer.compare(caches.get(a), caches.get(b));
+        }
+      }
+    )
+  }
 }
