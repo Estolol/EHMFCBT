fcf5685112fa67025bd8c051a669e50d8ce09498
Estolol <edouard.dufour-sans@polytechnique.edu>
2017-02-23 19:36:13 +0100

    videos n requests

---
+++ src/Request.java
@@ -0,0 +1,12 @@
+public class Request{
+  int video_id;
+  int endpoint_id;
+  int number;
+
+  public Request(int videos_id, int endpoint_id, int number){
+    this.videos_id = videos_id;
+    this.endpoint_id = endpoint_id;
+    this.number = number;
+  }
+
+}
