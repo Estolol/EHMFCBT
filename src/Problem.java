861bcc7007ce3325775223417184dfbb8753789a
lshao <laetitia.shao@gmail.com>
2017-02-23 18:56:36 +0100

    Input reading + update .gitignore

--- src/Problem.java
+++ src/Problem.java
@@ -8,10 +8,38 @@ import java.lang.Math;
 
 public class Problem{
 
-  String lol;
+  int V, E, R, C, X, Ld, K;
+  int[] S, c, Lc, Rv, Re, Rn;
 
   public Problem(Scanner in){
-    this.lol = in.next();
+    this.V = in.nextInt();
+    System.out.println(V);
+    this.E = in.nextInt();
+    this.R = in.nextInt();
+    this.C = in.nextInt();
+    this.X = in.nextInt();
+    S = new int[this.V];
+    for (int i = 0; i < V; i ++) {
+      S[i] = in.nextInt();
+    }
+
+    this.Ld = in.nextInt();
+    this.K = in.nextInt();
+    c = new int[K];
+    Lc = new int[K];
+    for (int i = 0; i < K; i++) {
+      c[i] = in.nextInt();
+      Lc[i] = in.nextInt();
+    }
+
+    Rv = new int[R];
+    Re = new int[R];
+    Rn = new int[R];
+    for (int i = 0; i < R; i++) {
+      Rv[i] = in.nextInt();
+      Re[i] = in.nextInt();
+      Rn[i] = in.nextInt();
+    }
   }
 
   public void solve(String output){
@@ -23,7 +51,7 @@ public class Problem{
     try {
 			PrintWriter writer = new PrintWriter(output , "UTF-8");
       writer.println("coucou");
-      
+
       writer.close();
 
     } catch (FileNotFoundException e) {
